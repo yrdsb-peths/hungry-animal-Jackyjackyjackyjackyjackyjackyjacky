4442116d54a4350edb7f433fcb7abb17b5e84521
Jackyjackyjackyjackyjackyjackyjacky <349922815@gapps.yrdsb.ca>
2024-09-22 21:40:59 -0400

    elephant animation

--- Elephant.java
+++ Elephant.java
@@ -2,6 +2,22 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class Elephant extends Actor
 {
+    GreenfootSound elephantSound = new GreenfootSound("elephantcub.mp3");
+    GreenfootImage[] idle = new GreenfootImage[8];
+    
+    public Elephant(){
+        for(int i = 0; i < idle.length; i++)
+        {
+            idle[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+            idle[i].scale(100,100);
+        }
+        setImage(idle[0]);
+    }
+    int imageIndex = 0;
+    public void animateElephant(){
+        setImage(idle[imageIndex]);
+        imageIndex = (imageIndex + 1) % idle.length;
+    }
     public void act()
     {
         if(Greenfoot.isKeyDown("left")){
@@ -10,11 +26,16 @@ public class Elephant extends Actor
         else if(Greenfoot.isKeyDown("right")){
             move(5);
         }
-        if(isTouching(Apple.class)){
+        eat();
+        animateElephant();
+    }
+    public void eat(){
+            if(isTouching(Apple.class)){
             removeTouching(Apple.class);
             MyWorld world = (MyWorld) getWorld();
             world.createApple();
             world.increaseScore();
+            elephantSound.play();
         }
     }
 }
