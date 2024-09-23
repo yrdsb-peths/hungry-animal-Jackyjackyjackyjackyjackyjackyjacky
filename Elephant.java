1c0c9325d55bc0916db72cdd4b0c753c64402cc5
Jackyjackyjackyjackyjackyjackyjacky <349922815@gapps.yrdsb.ca>
2024-09-22 21:59:21 -0400

    timed animation

--- Elephant.java
+++ Elephant.java
@@ -5,9 +5,8 @@ public class Elephant extends Actor
     GreenfootSound elephantSound = new GreenfootSound("elephantcub.mp3");
     GreenfootImage[] idleRight = new GreenfootImage[8];
     GreenfootImage[] idleLeft = new GreenfootImage[8];
-
-    
     String facing = "right";
+    SimpleTimer animationTimer = new SimpleTimer();
     
     public Elephant(){
         for(int i = 0; i < idleRight.length; i++)
@@ -21,9 +20,14 @@ public class Elephant extends Actor
             idleLeft[i].mirrorHorizontally();
             idleLeft[i].scale(100,100);
         }
+        animationTimer.mark();
     }
     int imageIndex = 0;
     public void animateElephant(){
+        if(animationTimer.millisElapsed() < 100){
+            return;
+        }
+        animationTimer.mark();
         if(facing.equals("right")){
             setImage(idleRight[imageIndex]);
             imageIndex = (imageIndex + 1) % idleRight.length;
