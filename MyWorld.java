5a611208f9e7b9202a4442cfab11d1f77ad590a4
Jackyjackyjackyjackyjackyjackyjacky <349922815@gapps.yrdsb.ca>
2024-09-24 17:54:11 -0400

    speed levels

--- MyWorld.java
+++ MyWorld.java
@@ -10,6 +10,8 @@ public class MyWorld extends World
 {
     public int score = 0;
     Label label;
+    int level = 1;
+    
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
@@ -32,10 +34,15 @@ public class MyWorld extends World
     public void increaseScore(){
         score++;
         label.setValue(score);
+        if(score % 5 == 0)
+        {
+            level += 1;
+        }
     }
     
     public void createApple(){
         Apple apple = new Apple();
+        apple.setSpeed(level);
         int x = Greenfoot.getRandomNumber(600);
         int y = 0;
         addObject(apple, x, y);
